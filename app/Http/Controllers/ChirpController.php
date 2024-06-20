e28b7088e94c6a86471d80c4b4a2ae54157e606e
Kallan Chandra Roy <shuvoroy123ok@gmail.com>
2024-06-20 13:25:32 +0600

    create chirp

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,6 +2,14 @@
 
 namespace App\Http\Controllers;
 
+
+//use Illuminate\Http\Response;
+
+use Inertia\Inertia;
+use Inertia\Response;
+use Illuminate\Http\RedirectResponse;
+
+
 use App\Models\Chirp;
 use Illuminate\Http\Request;
 
@@ -10,9 +18,13 @@ class ChirpController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
-        //
+        //return response('Hello, World!');
+
+        return Inertia::render('Chirps/Index', [
+            //
+        ]);
     }
 
     /**
@@ -26,9 +38,15 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
